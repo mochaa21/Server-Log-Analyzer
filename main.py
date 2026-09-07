1a665b601d7050818cbdd36b02599f39fb5819b6
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-07 11:15:29 +0700

    fix: logical bug

--- main.py
+++ main.py
@@ -14,27 +14,28 @@ raw_api_response = """
 }
 """
 
-def timer_decorator(func):
+def timer_decorator(func: callable) -> callable:
     @functools.wraps(func)
-    def myinner(x):
+    def myinner(*args, **kwargs):
         print("Processing Data...")
         time.sleep(2.5)
         print("Process Complete!")
+        print(func(*args, **kwargs))
         return
     return myinner
 
 @timer_decorator
 def analyze_logs(data: str) -> list:
-    response = json.loads(raw_api_response)
-    node_1 = response['data']['node_1_logins']
-    node_2 = response['data']['node_2_logins']
-    banned_users = response['data']['banned_users']
-    dataset_1 = set(int(angka) for angka in node_1.strip().split(', '))
-    dataset_2 = set(int(angka) for angka in node_2.strip().split(', '))
-    list_banned = set(int(angka) for angka in banned_users)
-    new_data = dataset_1.intersection(dataset_2)
-    new_data_check = new_data.difference(list_banned)
-    complete_data = sorted(new_data_check)
+    response: str = json.loads(raw_api_response)
+    node_1: str = response['data']['node_1_logins']
+    node_2: str = response['data']['node_2_logins']
+    banned_users: list = response['data']['banned_users']
+    dataset_1: set = set(int(angka) for angka in node_1.strip().split(', '))
+    dataset_2: set = set(int(angka) for angka in node_2.strip().split(', '))
+    list_banned: set = set(int(angka) for angka in banned_users)
+    new_data: set = dataset_1.intersection(dataset_2)
+    new_data_check: set = new_data.difference(list_banned)
+    complete_data: list = sorted(new_data_check)
     return f"Valid players active on both nodes: {complete_data}"
 
 analyze_logs(raw_api_response)
