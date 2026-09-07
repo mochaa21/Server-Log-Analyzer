fe426d21f7a635f074c008e9bec4bb59975a379d
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-07 11:03:20 +0700

    fix: change operators

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 import time
+import functools
 from typing import Callable
 
-# Data simulasi dari server (berwujud String JSON)
 raw_api_response = """
 {
     "status": "success",
@@ -14,32 +14,28 @@ raw_api_response = """
 }
 """
 
-# TUGAS 1: Buat Decorator
-# Buat decorator bernama @timer_decorator yang mencetak "Memproses data..." 
-# sebelum fungsi berjalan, dan "Proses selesai!" setelah fungsi selesai.
 def timer_decorator(func):
-    def myinner():
-        print("Processing Data")
+    @functools.wraps(func)
+    def myinner(x):
+        print("Processing Data...")
         time.sleep(2.5)
         print("Process Complete!")
         return
     return myinner
 
-
-# TUGAS 2, 3, & 4: Fungsi Utama dengan Type Annotations
-# Buat fungsi analyze_logs yang menerima argumen string dan mereturn list.
-# Jangan lupa pakaikan jaket @timer_decorator di atasnya.
-def analyze_logs(data):
+@timer_decorator
+def analyze_logs(data: str) -> list:
     response = json.loads(raw_api_response)
     node_1 = response['data']['node_1_logins']
     node_2 = response['data']['node_2_logins']
     banned_users = response['data']['banned_users']
     dataset_1 = set(int(angka) for angka in node_1.strip().split(', '))
     dataset_2 = set(int(angka) for angka in node_2.strip().split(', '))
+    list_banned = set(int(angka) for angka in banned_users)
     new_data = dataset_1.intersection(dataset_2)
-    new_data_check = new_data.difference(banned_users)
-    complete_data = list(new_data_check)
-    return complete_data
+    new_data_check = new_data.difference(list_banned)
+    complete_data = sorted(new_data_check)
+    return f"Valid players active on both nodes: {complete_data}"
 
 analyze_logs(raw_api_response)
 
